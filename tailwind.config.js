752490b364850da351abf813d6223bb2803dcd6a
İzzettin <134144228+blueshdw@users.noreply.github.com>
2024-10-27 19:32:11 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "astreoids1": "url('images/bg-1.png')",
+        "astreoids2": "url('images/bg-2.png')",
+        "astreoids3": "url('images/bg-3.png')",
+        "astreoids4": "url('images/bg-4.png')",
+      },
+    },
   },
   plugins: [],
-}
+};
