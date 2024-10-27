a56566d77386a99870a9908c099414ccc4faeedb
İzzettin <134144228+blueshdw@users.noreply.github.com>
2024-10-27 19:31:27 +0300

    Update TheTeam.jsx

--- src/components/TheTeam.jsx
+++ src/components/TheTeam.jsx
@@ -3,14 +3,14 @@ import TeamData from "../TeamData.json";
 
 const TheTeam = () => {
   return (
-    <div className="min-h-96 py-10" id="theteam">
+    <div className="min-h-[600px] py-10 bg-astreoids3 bg-cover" id="theteam">
       <h1 className="text-5xl 2xl:text-7xl text-[#99C7E8] [text-shadow:_0_4px_8px_rgba(14_165_223_/_0.5)]">
         The Team
       </h1>
       <div className="mt-10 flex justify-center flex-wrap gap-4">
         {TeamData.map((user) => {
           return (
-            <div className="w-60 2xl:w-80 overflow-hidden flex flex-col" key={user.id}>
+            <div className="cursor-pointer hover:scale-105 transition-all w-60 2xl:w-80 overflow-hidden flex flex-col" key={user.id}>
               <img className="rounded-xl mb-2" src={user.profilePhoto} alt="" />
               <span className="text-[#D8DAE3] font-bold">{user.name}</span>
               <span className="text-[#D8DAE3] font-bold">-{user.jon}</span>
@@ -21,5 +21,7 @@ const TheTeam = () => {
     </div>
   );
 };
+
+export default TheTeam;
 
 export default TheTeam;
