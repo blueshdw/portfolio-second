9125c3d3140d66bd309b02e9cd934b4fa4ed8f1a
İzzettin <134144228+blueshdw@users.noreply.github.com>
2024-10-27 19:31:46 +0300

    Update Reach.jsx

--- src/components/Reach.jsx
+++ src/components/Reach.jsx
@@ -9,13 +9,13 @@ const Reach = () => {
     window.location.href = "mailto:sertanmokcu@gmail.com";
   };
   return (
-    <div className="min-h-96" id="reaches">
+    <div className="min-h-96 bg-astreoids4" id="reaches">
       <h1 className="text-5xl 2xl:text-7xl text-[#99C7E8] [text-shadow:_0_4px_8px_rgba(14_165_223_/_0.5)]">
         2 Reach
       </h1>
       <div className="mt-2 flex justify-center items-center flex-wrap gap-4">
         <img
-          className="cursor-pointer"
+          className="cursor-pointer hover:scale-110 transition-all"
           src={gmail}
           alt=""
           onClick={() => windowLocationHref()}
@@ -24,6 +24,7 @@ const Reach = () => {
           href="https://github.com/SertanM"
           rel="noopener noreferrer"
           target="_blank"
+          className="cursor-pointer hover:scale-110 transition-all"
         >
           <img className="cursor-pointer" src={github} alt="" />
         </a>
@@ -31,6 +32,7 @@ const Reach = () => {
           href="https://sertanm.itch.io/"
           rel="noopener noreferrer"
           target="_blank"
+          className="cursor-pointer hover:scale-110 transition-all"
         >
           <img className="cursor-pointer" src={itch} alt="" />
         </a>
@@ -38,6 +40,7 @@ const Reach = () => {
           href="https://www.linkedin.com/in/sertan-m-okcu-797a53306/?originalSubdomain=tr"
           rel="noopener noreferrer"
           target="_blank"
+          className="cursor-pointer hover:scale-110 transition-all"
         >
           <img className="cursor-pointer" src={linkedin} alt="" />
         </a>
